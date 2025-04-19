350d5924e5f958ca3f8a6cf3779ec6ae2576c1cd
mzal <58111339+mzwrt@users.noreply.github.com>
2025-04-20 00:36:24 +0800

    Update install-xray.sh

--- xray/install-xray.sh
+++ xray/install-xray.sh
@@ -59,7 +59,7 @@ ExecStartPre=/bin/bash -c "ulimit -n 65536"  # 设置最大文件描述符数量
 ExecStart=$INSTALL_DIR/xray -config $INSTALL_DIR/config.json
 Restart=on-failure
 LimitNOFILE=65536  # 设置最大文件描述符数量为 65536
-LimitNPROC=1024    # 设置最大进程数（可选）
+LimitNPROC=65536    # 设置最大进程数（可选）
 LimitCORE=infinity # 设置允许的核心转储大小（可选）
 LimitSTACK=8192k   # 设置栈大小（可选）
 
