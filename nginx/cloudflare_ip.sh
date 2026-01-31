981ece1cf204f9f66113407324093ae55af08078
mzal <58111339+mzwrt@users.noreply.github.com>
2026-02-01 04:01:55 +0800

    Update cloudflare_ip.sh

--- nginx/cloudflare_ip.sh
+++ nginx/cloudflare_ip.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 # 定期获取 cloudflare 的IP让nginx可以获取到用户真实IP
-rm %NGINX_DIR%/conf.d/sites-available/cloudflare_ip.conf
+if [ -f %NGINX_DIR%/conf.d/sites-available/cloudflare_ip.conf ]; then
+    rm %NGINX_DIR%/conf.d/sites-available/cloudflare_ip.conf
+fi
 echo "#Cloudflare" > %NGINX_DIR%/conf.d/sites-available/cloudflare_ip.conf;
 for i in `curl https://www.cloudflare.com/ips-v4`; do
         echo "set_real_ip_from $i;" >> %NGINX_DIR%/conf.d/sites-available/cloudflare_ip.conf;
