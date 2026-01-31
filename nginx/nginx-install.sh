eb03235a9036ffe8b2960a51a348c4dd291d261d
mzal <58111339+mzwrt@users.noreply.github.com>
2026-02-01 05:19:12 +0800

    Update nginx-install.sh

--- nginx/nginx-install.sh
+++ nginx/nginx-install.sh
@@ -776,8 +776,19 @@ fi
   chmod +x "/root/cloudflare_ip.sh"
   chmod 600 "/root/cloudflare_ip.sh"
   chown root:root "/root/cloudflare_ip.sh"
-  # 运行 cloudflare_ip.sh 脚本
-  bash /root/cloudflare_ip.sh
+  # 运行 添加获取真实IP文件
+   echo "#Cloudflare" > $NGINX_DIR/conf.d/sites-available/cloudflare_ip.conf;
+  for i in `curl https://www.cloudflare.com/ips-v4`; do
+          echo "set_real_ip_from $i;" >> $NGINX_DIR/conf.d/sites-available/cloudflare_ip.conf;
+  done
+  for i in `curl https://www.cloudflare.com/ips-v6`; do
+          echo "set_real_ip_from $i;" >> $NGINX_DIR/conf.d/sites-available/cloudflare_ip.conf;
+  done
+  echo "" >> $NGINX_DIR/conf.d/sites-available/cloudflare_ip.conf;
+  echo "# use any of the following two" >> $NGINX_DIR/conf.d/sites-available/cloudflare_ip.conf;
+  echo "real_ip_header CF-Connecting-IP;" >> $NGINX_DIR/conf.d/sites-available/cloudflare_ip.conf;
+  echo "#real_ip_header X-Forwarded-For;" >> $NGINX_DIR/conf.d/sites-available/cloudflare_ip.conf;
+  chmod 600 $NGINX_DIR/conf.d/sites-available/cloudflare_ip.conf
   # 添加每月1号执行的定时任务
   echo "0 0 1 * * /root/cloudflare_ip.sh && (crontab -l | grep -v '/root/cloudflare_ip.sh' | crontab -)" | crontab -
 
