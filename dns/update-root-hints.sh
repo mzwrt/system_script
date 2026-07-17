679220eeedc143b804a9ffc65e00d32248994cd7
HuangYao <58111339+mzwrt@users.noreply.github.com>
2026-07-18 07:51:54 +0800

    Update update-root-hints.sh

--- dns/update-root-hints.sh
+++ dns/update-root-hints.sh
@@ -1,16 +1,27 @@
 #!/bin/bash
 
+exec 9>/run/lock/unbound-root-update.lock
+flock -n 9 || exit 1
+
 ROOT_HINTS="/var/lib/unbound/root.hints"
 ROOT_KEY="/var/lib/unbound/root.key"
 
 curl -fsSL https://www.internic.net/domain/named.cache \
 -o ${ROOT_HINTS}.new || exit 1
 
-mv ${ROOT_HINTS}.new ${ROOT_HINTS}
+if [ -s ${ROOT_HINTS}.new ]; then
+    mv ${ROOT_HINTS}.new ${ROOT_HINTS}
+else
+    exit 1
+fi
 
-unbound-anchor -a ${ROOT_KEY}
+unbound-anchor -a ${ROOT_KEY} -v
 
-chown unbound:unbound ${ROOT_HINTS} ${ROOT_KEY}
+chown unbound:unbound ${ROOT_HINTS}
+chmod 644 ${ROOT_HINTS}
+
+chown unbound:unbound ${ROOT_KEY}
+chmod 600 ${ROOT_KEY}
 
 unbound-control reload_keep_cache
 
