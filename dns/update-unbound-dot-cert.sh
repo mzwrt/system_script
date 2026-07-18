0382292f7eac8f2772baf3de6bd081d9431dae28
HuangYao <58111339+mzwrt@users.noreply.github.com>
2026-07-18 10:31:56 +0800

    Update update-unbound-dot-cert.sh

--- dns/update-unbound-dot-cert.sh
+++ dns/update-unbound-dot-cert.sh
@@ -1,26 +1,79 @@
 #!/bin/bash
 
 SRC="/opt/nginx/ssl/mzwrt.com"
-DST="/etc/unbound/tls"
+
+UNBOUND_DST="/etc/unbound/tls"
+DNSDIST_DST="/etc/dnsdist/certs"
 
 CERT="${SRC}/fullchain.pem"
 KEY="${SRC}/privkey.pem"
 
+
 if [ ! -f "$CERT" ] || [ ! -f "$KEY" ]; then
-    logger -t unbound-dot "Certificate files missing"
+    logger -t dns-cert "Certificate files missing"
     exit 1
 fi
 
-mkdir -p "$DST"
 
-cp -f "$CERT" "${DST}/cert.pem"
-cp -f "$KEY" "${DST}/key.pem"
+########################
+# Unbound DoT
+########################
 
-chown root:unbound "${DST}/cert.pem" "${DST}/key.pem"
+mkdir -p "$UNBOUND_DST"
 
-chmod 644 "${DST}/cert.pem"
-chmod 600 "${DST}/key.pem"
 
-unbound-control reload
+cp "$CERT" "${UNBOUND_DST}/cert.pem.new"
+cp "$KEY" "${UNBOUND_DST}/key.pem.new"
 
-logger -t unbound-dot "DoT certificate updated and Unbound reloaded"
+
+mv "${UNBOUND_DST}/cert.pem.new" \
+   "${UNBOUND_DST}/cert.pem"
+
+mv "${UNBOUND_DST}/key.pem.new" \
+   "${UNBOUND_DST}/key.pem"
+
+
+chown root:unbound \
+"${UNBOUND_DST}/cert.pem" \
+"${UNBOUND_DST}/key.pem"
+
+
+chmod 644 "${UNBOUND_DST}/cert.pem"
+chmod 640 "${UNBOUND_DST}/key.pem"
+
+
+systemctl restart unbound
+
+
+
+########################
+# dnsdist DoH
+########################
+
+mkdir -p "$DNSDIST_DST"
+
+
+cp "$CERT" "${DNSDIST_DST}/cert.pem.new"
+cp "$KEY" "${DNSDIST_DST}/key.pem.new"
+
+
+mv "${DNSDIST_DST}/cert.pem.new" \
+   "${DNSDIST_DST}/cert.pem"
+
+mv "${DNSDIST_DST}/key.pem.new" \
+   "${DNSDIST_DST}/key.pem"
+
+
+chown _dnsdist:_dnsdist \
+"${DNSDIST_DST}/cert.pem" \
+"${DNSDIST_DST}/key.pem"
+
+
+chmod 644 "${DNSDIST_DST}/cert.pem"
+chmod 600 "${DNSDIST_DST}/key.pem"
+
+
+systemctl restart dnsdist
+
+
+logger -t dns-cert "DoT and DoH certificates updated"
