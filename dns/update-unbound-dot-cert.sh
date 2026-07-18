93333f5db17fe91d6a7a496b4f4d5d5ff776b83d
HuangYao <58111339+mzwrt@users.noreply.github.com>
2026-07-18 10:34:50 +0800

    Update update-unbound-dot-cert.sh

--- dns/update-unbound-dot-cert.sh
+++ dns/update-unbound-dot-cert.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
+set -e
 
-SRC="/opt/nginx/ssl/mzwrt.com"
+SRC="/opt/nginx/ssl/ihccc.com"
 
 UNBOUND_DST="/etc/unbound/tls"
 DNSDIST_DST="/etc/dnsdist/certs"
@@ -9,71 +10,55 @@ CERT="${SRC}/fullchain.pem"
 KEY="${SRC}/privkey.pem"
 
 
-if [ ! -f "$CERT" ] || [ ! -f "$KEY" ]; then
-    logger -t dns-cert "Certificate files missing"
+if [ ! -f "$CERT" ]; then
+    logger -t dns-cert "Certificate missing: $CERT"
+    exit 1
+fi
+
+if [ ! -f "$KEY" ]; then
+    logger -t dns-cert "Key missing: $KEY"
     exit 1
 fi
 
 
-########################
+############################
 # Unbound DoT
-########################
+############################
 
 mkdir -p "$UNBOUND_DST"
 
-
-cp "$CERT" "${UNBOUND_DST}/cert.pem.new"
-cp "$KEY" "${UNBOUND_DST}/key.pem.new"
-
-
-mv "${UNBOUND_DST}/cert.pem.new" \
-   "${UNBOUND_DST}/cert.pem"
-
-mv "${UNBOUND_DST}/key.pem.new" \
-   "${UNBOUND_DST}/key.pem"
-
+cp -f "$CERT" "$UNBOUND_DST/cert.pem"
+cp -f "$KEY" "$UNBOUND_DST/key.pem"
 
 chown root:unbound \
-"${UNBOUND_DST}/cert.pem" \
-"${UNBOUND_DST}/key.pem"
+"$UNBOUND_DST/cert.pem" \
+"$UNBOUND_DST/key.pem"
+
+chmod 644 "$UNBOUND_DST/cert.pem"
+chmod 640 "$UNBOUND_DST/key.pem"
 
 
-chmod 644 "${UNBOUND_DST}/cert.pem"
-chmod 640 "${UNBOUND_DST}/key.pem"
-
-
-systemctl restart unbound
-
-
-
-########################
+############################
 # dnsdist DoH
-########################
+############################
 
 mkdir -p "$DNSDIST_DST"
 
-
-cp "$CERT" "${DNSDIST_DST}/cert.pem.new"
-cp "$KEY" "${DNSDIST_DST}/key.pem.new"
-
-
-mv "${DNSDIST_DST}/cert.pem.new" \
-   "${DNSDIST_DST}/cert.pem"
-
-mv "${DNSDIST_DST}/key.pem.new" \
-   "${DNSDIST_DST}/key.pem"
+cp -f "$CERT" "$DNSDIST_DST/cert.pem"
+cp -f "$KEY" "$DNSDIST_DST/key.pem"
 
 
 chown _dnsdist:_dnsdist \
-"${DNSDIST_DST}/cert.pem" \
-"${DNSDIST_DST}/key.pem"
+"$DNSDIST_DST/cert.pem" \
+"$DNSDIST_DST/key.pem"
+
+chmod 644 "$DNSDIST_DST/cert.pem"
+chmod 600 "$DNSDIST_DST/key.pem"
 
 
-chmod 644 "${DNSDIST_DST}/cert.pem"
-chmod 600 "${DNSDIST_DST}/key.pem"
 
-
+systemctl reload unbound || true
 systemctl restart dnsdist
 
 
-logger -t dns-cert "DoT and DoH certificates updated"
+logger -t dns-cert "Certificate updated for Unbound and dnsdist"
